64b4e7ce462f2be574dd2779ccc119667a101de2
Andres Jimenez <andres_jimenez666@hotmail.com>
2024-11-24 16:28:49 -0500

    Segundo comit, edicion de front, styles

---
+++ BusReservation_Maui/Constants.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace BusReservation_Maui
+{
+     public static class Constants
+     {
+          private const string DBFileName = "ProsperDaily.db3";
+
+          public const SQLiteOpenFlags Flags =
+               SQLiteOpenFlags.ReadWrite |
+               SQLiteOpenFlags.Create |
+               SQLiteOpenFlags.SharedCache;
+
+          public static string DatabasePath
+          {
+               get
+               {
+                    return Path
+                         .Combine(FileSystem.AppDataDirectory, DBFileName);
+               }
+          }
+     }
+}
--- BusReservation_Maui/Platforms/iOS/AppDelegate.cs
+++ BusReservation_Maui/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace BusReservation_Maui;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
